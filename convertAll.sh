2151a855f8ffbbe45b6aade8fad49a7aa139b08f
psycho <psychowoife@gmail.com>
2016-03-15 19:23:18 +0100

    added README

--- convertAll.sh
+++ convertAll.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-mkdir -p png
-
-for i in out/*.dat;
-do k=${i##*/}
-j="png/${k%.dat}"
-gnuplot -e "inputfilename='${i}'; outputfilename='${j}'" 3DJulia2png;
-done
+if [ "$1" == png ];
+	then 	
+		mkdir -p png
+		for i in out/*.dat;
+		do 
+			k=${i##*/}
+			j="png/${k%.dat}"
+			gnuplot -e "inputfilename='${i}'; outputfilename='${j}'" 3DJulia2png;
+		done
+fi
+if [ "$1" == eps ]:
+	then 	
+		mkdir -p eps
+		for i in out/*.dat;
+		do 
+			k=${i##*/}
+			j="eps/${k%.dat}"
+			gnuplot -e "inputfilename='${i}'; outputfilename='${j}'" 3DJulia2eps;
+		done
+fi
